d64d3c41237785a836e55a539d6ac7f776c3d6c1
Daegon13 <damgmarin13@gmail.com>
2025-10-06 00:28:08 -0300

    Proyecto añadido

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,6 @@
 
 export default new Map([
-["src/content/proyectos/cristal-sagrado.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado.mdx&astroContentModuleFlag=true")]]);
+["src/content/proyectos/cristal-sagrado.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado.mdx&astroContentModuleFlag=true")],
+["src/content/proyectos/cristal-sagrado copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado+copy.mdx&astroContentModuleFlag=true")],
+["src/content/proyectos/Agencia_ariel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2FAgencia_ariel.mdx&astroContentModuleFlag=true")]]);
 		
