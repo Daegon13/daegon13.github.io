39e6065e6697d5a2762538c0b35e4ba36faf4aad
Daegon13 <damgmarin13@gmail.com>
2025-10-05 21:21:22 -0300

    Pagina desplegada con exito

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,1 @@
-
-export default new Map([
-["src/content/proyectos/cristal-sagrado.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
