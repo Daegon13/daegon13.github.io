59100ebbc711ae99f46b9981ddca1c2a3faa45ed
Daegon13 <damgmarin13@gmail.com>
2026-02-07 17:07:51 -0300

    Mejoras de diseño

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
+["src/content/proyectos/Agencia_ariel copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2FAgencia_ariel+copy.mdx&astroContentModuleFlag=true")],
 ["src/content/proyectos/Agencia_ariel.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2FAgencia_ariel.mdx&astroContentModuleFlag=true")],
 ["src/content/proyectos/servicio-de-tarot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fservicio-de-tarot.mdx&astroContentModuleFlag=true")],
-["src/content/proyectos/cristal-sagrado.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado.mdx&astroContentModuleFlag=true")],
-["src/content/proyectos/Agencia_ariel copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2FAgencia_ariel+copy.mdx&astroContentModuleFlag=true")]]);
+["src/content/proyectos/cristal-sagrado.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fproyectos%2Fcristal-sagrado.mdx&astroContentModuleFlag=true")]]);
 		
